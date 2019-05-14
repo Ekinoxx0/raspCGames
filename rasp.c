df0ac89b623ecb61147d623dc539747310bbd090
Ekinoxx <fadow0711@gmail.com>
2019-05-14 18:32:04 +0200

    Update rasp.c (New games)

--- rasp.c
+++ rasp.c
@@ -2,6 +2,8 @@
 #include <wiringPi.h>
 #include <math.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 #define BG_1 7
 #define BG_2 0
 #define BG_3 1
@@ -37,7 +39,8 @@ void writeRedLed(int state);
 void toggleGreenLed();
 void toggleRedLed();
 void writeBargraph(unsigned char stateLedsBG);
-void writeBargraphUniLed();
+void writeBargraphLed(int led);
+void writeBargraphUniLed(int led);
 void blankBargraph();
 void fullBargraph();
 int readLeftButton();
@@ -53,6 +56,7 @@ long double powl(long double x, long double y);
 void changeGamemode(int gamemode);
 void beforeGame();
 void afterGame();
+void displayPoints();
 
 //Corps
 
@@ -65,6 +69,9 @@ int sens = 0;
 
 int pointPlayer1 = 0;
 int pointPlayer2 = 0;
+
+int rouletteSpeed = 0;
+int rouletteCursor = 0;
 
 
 int main(void)
@@ -100,7 +107,6 @@ int main(void)
 					sens = 0;
 				}
 				writeBargraphUniLed(gameCursor+1);
-				printf("Loop\n");
 
 				delay(60);
 				break;
@@ -114,6 +120,26 @@ int main(void)
 
 
 			case ROULETTE :
+				if(gameState == 0){
+					writeBargraphUniLed(gameCursor+1);
+				} else if(gameState == 1){
+					rouletteCursor++;
+					if(rouletteCursor > 7){
+						rouletteCursor = 0;
+					}
+					rouletteSpeed *= 1.1;
+
+					blankBargraph();
+					writeBargraphLed(gameCursor+1);
+					writeBargraphLed(rouletteCursor+1);
+
+					if(rouletteSpeed > 600){
+						afterGame();
+						continue;
+					}
+
+					delay(rouletteSpeed);
+				}
 				break;
 
 
@@ -137,9 +163,15 @@ int main(void)
 void changeGamemode(int gamemode){
 	printf("Changement de mode de jeu de %d pour %d \n", generalState, gamemode);
 	generalState = gamemode;
+	gameState = 0;
+	gameCursor = 0;
+	sens = 0;
+	pointPlayer1 = 0;
+	pointPlayer2 = 0;
 
 	switch(generalState) {
 		case MENU :
+			printf("MENU PRINCIPAL !\n");
 			writeBargraphUniLed(gameCursor);
 			writeGreenLed(1);
 			writeRedLed(1);
@@ -147,16 +179,33 @@ void changeGamemode(int gamemode){
 			break;
 
 		case CATCH_ME :
-			gameState = 0;
-			gameCursor = 5;
-			fullBargraph();
-			delay(1000);
+			printf("Bienvenue dans CATCH ME !\n");
+			printf("Mode de jeu : 2 joueurs\n");
+			printf("Nombre de point à obtenir : 4points\n");
+			printf("Explication: Chaque joueur prend un bouton,\n");
+			printf("             il doit appuyer sur le bouton quand le lumière est de son coté,\n");
+			printf("             si il réussi, la led vert s'allume\n");
+			printf("             si il échoue, la led rouge s'allume.\n");
+			printf("             à la fin de chaque manche le nombre de point de chaque joueur s'affiche\n");
+			beforeGame();
 			break;
 
 		case FILLING_MORE :
-			writeRedLed(1);
-			writeGreenLed(0);
-			blankBargraph();
+			printf("Bienvenue dans CATCH ME !\n");
+			printf("JEU PAS FINI\n");
+			beforeGame();
+			break;
+
+		case SIMON :
+			printf("Bienvenue dans SIMON!\n");
+			printf("JEU PAS FINI\n");
+			beforeGame();
+			break;
+
+		case ROULETTE :
+			printf("Bienvenue dans ROULETTE !\n");
+			printf("Mode de jeu : 1 joueur\n");
+			beforeGame();
 			break;
 
 		default : 
@@ -166,11 +215,17 @@ void changeGamemode(int gamemode){
 }
 
 void beforeGame(){
+	writeGreenLed(0);
+	writeRedLed(0);
+
 	switch(generalState) {
 		case MENU :
 			break;
 
 		case CATCH_ME :
+				gameState = 0;
+				gameCursor = 4;
+				sens = rand() % 2;
 				fullBargraph();
 				delay(200);
 				writeBargraph(0b11111100);
@@ -182,6 +237,20 @@ void beforeGame(){
 			break;
 
 		case FILLING_MORE :
+			delay(3000);
+			changeGamemode(MENU);
+			break;
+
+		case SIMON :
+			delay(3000);
+			changeGamemode(MENU);
+			break;
+
+		case ROULETTE :
+			rouletteSpeed = 20;
+			gameCursor = 0;
+			rouletteCursor = 0;
+			printf("Sélectionnez votre case...\n");
 			break;
 
 		default : 
@@ -196,28 +265,55 @@ void afterGame(){
 			break;
 
 		case CATCH_ME :
-				printf("afterGame called by interupt\n");
-				fullBargraph();
-				delay(200);
 				if(gameCursor == 1){
+					printf("Bien joué !\n");
 					writeGreenLed(1);
 					writeRedLed(0);
 				} else {
+					printf("Raté\n");
 					writeGreenLed(0);
 					writeRedLed(1);
 				}
 				sleep(2);
-				writeGreenLed(0);
-				writeRedLed(0);
-				printf("pointPlayer1 : %d\n", pointPlayer1);
-				printf("pointPlayer2 : %d\n", pointPlayer1);
-				int total = 0;
-				writeBargraph(0b00000000);
-				delay(1500);
-				printf("afterGame ended\n");
+
+				displayPoints();
+
+				if(pointPlayer1 > 3 || pointPlayer2 > 3){
+					changeGamemode(MENU);
+					return;
+				}
 			break;
 
 		case FILLING_MORE :
+			break;
+
+		case SIMON :
+			break;
+
+		case ROULETTE :
+			if(gameCursor == rouletteCursor){
+				for(int i = 0; i < 3; i++){
+					writeBargraphLed(rouletteCursor+1);
+					blankBargraph();
+					delay(300);
+				}
+				printf("Bien joué !\n");
+				writeGreenLed(1);
+				writeRedLed(0);
+				pointPlayer1++;
+			} else {
+				printf("Raté\n");
+				writeGreenLed(0);
+				writeRedLed(1);
+			}
+			sleep(2);
+
+			displayPoints();
+
+			if(pointPlayer1 > 3){
+				changeGamemode(MENU);
+			}
+			gameState = 0;
 			break;
 
 		default : 
@@ -228,16 +324,73 @@ void afterGame(){
 	beforeGame();
 }
 
+void displayPoints(){
+	writeGreenLed(0);
+	writeRedLed(0);
+	printf("pointPlayer1 : %d\n", pointPlayer1);
+	printf("pointPlayer2 : %d\n", pointPlayer2);
+
+	int total = 0;
+
+	if(pointPlayer2 >= 1){
+		total += 1;
+	}
+	if(pointPlayer2 >= 2){
+		total += 2;
+	}
+	if(pointPlayer2 >= 3){
+		total += 4;
+	}
+	if(pointPlayer2 >= 4){
+		for(int i = 0; i < 4; i++){
+			blankBargraph();
+			delay(300);
+			writeBargraph(0b00001111);
+			delay(300);
+		}
+		delay(1000);
+		return;
+	}
+
+	if(pointPlayer1 >= 1){
+		total += 128;
+	}
+	if(pointPlayer1 >= 2){
+		total += 64;
+	}
+	if(pointPlayer1 >= 3){
+		total += 32;
+	}
+	if(pointPlayer1 >= 4){
+		for(int i = 0; i < 4; i++){
+			blankBargraph();
+			delay(300);
+			writeBargraph(0b11110000);
+			delay(300);
+		}
+		delay(1000);
+		return;
+	}
+
+	writeBargraph(total);
+
+	delay(1500);
+}
+
 
 //
 // Get Buttonned
 //
 
 void rightButtonned(){
+	if(interuptHandling == 1){
+		return;
+	}
 	interuptHandling = 1;
 
 	switch(generalState) {
 		case MENU :
+			printf("Sélection mode de jeu suivant, cliquez sur l'autre bouton pour confirmer.\n");
 			gameCursor++;
 			if (gameCursor > GAMES_NUMBER-1){ 
 				gameCursor = 0;
@@ -245,8 +398,9 @@ void rightButtonned(){
 			break;
 
 		case CATCH_ME :
-			printf("RIGHT : %d / %d\n", gameCursor, sens);
+			printf("Bouton appuyé par joueur 1 !\n");
 			if(gameCursor == 7){
+				pointPlayer1++;
 				gameCursor = 1;
 			} else {
 				gameCursor = 0;
@@ -255,6 +409,16 @@ void rightButtonned(){
 			break;
 
 		case FILLING_MORE :
+			break;
+
+		case SIMON:
+			break;
+
+		case ROULETTE:
+			gameCursor++;
+			if (gameCursor > 7){ 
+				gameCursor = 0;
+			}
 			break;
 
 		default : 
@@ -265,6 +429,9 @@ void rightButtonned(){
 }
 
 void leftButtonned(){
+	if(interuptHandling == 1){
+		return;
+	}
 	interuptHandling = 1;
 
 	switch(generalState) {
@@ -273,8 +440,9 @@ void leftButtonned(){
 			break;
 			
 		case CATCH_ME :
-			printf("LEFT : %d / %d\n", gameCursor, sens);
-			if(gameCursor == GAMES_NUMBER-1){
+			printf("Bouton appuyé par joueur 2 !\n");
+			if(gameCursor == 0){
+				pointPlayer2++;
 				gameCursor = 1;
 			} else {
 				gameCursor = 0;
@@ -285,11 +453,27 @@ void leftButtonned(){
 		case FILLING_MORE :
 			break;
 
+		case SIMON:
+			break;
+
+		case ROULETTE:
+			printf("Lancement de la roulette !\n");
+			gameState = 1;
+			break;
+
 		default : 
 			printf("erreur interne du logiciel #2\n");
 	}
 
 	interuptHandling = 0;
+}
+
+void rightButtonnedUp(){
+
+}
+
+void leftButtonnedUp(){
+	
 }
 
 
@@ -326,6 +510,10 @@ void writeBargraph(unsigned char stateLedsBG){
 		int filtre = pow(2,i); 
         	digitalWrite(pinBargraph[i], stateLedsBG & filtre);
 	}
+}
+
+void writeBargraphLed(int uniLed){
+	writeBargraph(powl(2, uniLed-1));
 }
 
 void writeBargraphUniLed(int uniLed){
@@ -368,5 +556,3 @@ int readLeftButton(){
 int readRightButton(){
 	return digitalRead(BUTTON_RIGHT) == 0 ? 1 : 0;
 }
-
-
